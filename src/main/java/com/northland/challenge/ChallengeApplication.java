4abc5156bb58940e76ce0923e86f2e67f09a27df
Anil Muthukrishnan <anil.muthukrishnan@mdacorporation.com>
2020-09-02 17:40:44 -0700

    Change to Gradle

--- src/main/java/com/northland/challenge/ChallengeApplication.java
+++ src/main/java/com/northland/challenge/ChallengeApplication.java
@@ -2,8 +2,14 @@ package com.northland.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api")
 public class ChallengeApplication {
 
 	public static void main(String[] args) {
